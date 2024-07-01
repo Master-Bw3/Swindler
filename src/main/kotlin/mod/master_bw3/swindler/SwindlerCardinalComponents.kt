8232c66ccecf42ded1a5066a35bd9069fdcedde8
Master-Bw3 <bw1jochman@gmail.com>
2024-07-01 12:56:45 -0500

    simplify components

--- src/main/kotlin/mod/master_bw3/swindler/SwindlerCardinalComponents.kt
+++ src/main/kotlin/mod/master_bw3/swindler/SwindlerCardinalComponents.kt
@@ -1,10 +1,19 @@
 package mod.master_bw3.swindler
 
-import mod.master_bw3.swindler.entity.SpellEffectComponent
-import mod.master_bw3.swindler.entity.SpellComponent
+import com.mojang.datafixers.util.Pair
+import dev.enjarai.trickster.spell.SpellPart
+import mod.master_bw3.swindler.SwindlerCardinalComponents.SPELL
+import mod.master_bw3.swindler.SwindlerCardinalComponents.SPELL_EFFECT
 import mod.master_bw3.swindler.entity.SpellEmitterEntity
+import net.minecraft.entity.Entity
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.nbt.NbtOps
+import net.minecraft.registry.RegistryWrapper
+import net.minecraft.util.Identifier
 import org.ladysnake.cca.api.v3.component.ComponentKey
 import org.ladysnake.cca.api.v3.component.ComponentRegistry
+import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent
 import org.ladysnake.cca.api.v3.entity.EntityComponentFactoryRegistry
 import org.ladysnake.cca.api.v3.entity.EntityComponentInitializer
 
@@ -24,13 +33,74 @@ object SwindlerCardinalComponents : EntityComponentInitializer {
     override fun registerEntityComponentFactories(registry: EntityComponentFactoryRegistry) {
         registry.registerFor(
             SpellEmitterEntity::class.java,
-            SPELL
-        ) { SpellComponent() }
+            SPELL,
+            ::SpellComponent
+        )
 
         registry.registerFor(
             SpellEmitterEntity::class.java,
-            SPELL_EFFECT
-        ) { SpellEffectComponent() }
+            SPELL_EFFECT,
+            ::SpellEffectComponent
+        )
     }
 
+}
+
+
+class SpellComponent(private val entity: Entity) : AutoSyncedComponent {
+    var spell: SpellPart = SpellPart()
+        set(value) {
+            field = value
+            SPELL.sync(entity)
+        }
+
+    override fun readFromNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
+        if (tag.contains(Swindler.id("spell").toString())) {
+            SpellPart.CODEC.decode(NbtOps.INSTANCE, tag[Swindler.id("spell").toString()])
+                .resultOrPartial()
+                .ifPresent { pair: Pair<SpellPart, NbtElement> ->
+                    spell = pair.first
+                }
+        }
+    }
+
+    override fun writeToNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
+        SpellPart.CODEC.encodeStart(NbtOps.INSTANCE, spell)
+            .resultOrPartial()
+            .ifPresent { element: NbtElement? -> tag.put(Swindler.id("spell").toString(), element) }
+    }
+}
+
+class SpellEffectComponent(private val entity: Entity) : AutoSyncedComponent {
+    var effectId: Identifier = Swindler.id("none")
+        set(value) {
+            field = value
+            SPELL_EFFECT.sync(entity)
+        }
+
+    var effectData: NbtCompound = NbtCompound()
+        set(value) {
+            field = value
+            SPELL_EFFECT.sync(entity)
+        }
+
+    override fun readFromNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
+        if (tag.contains(Swindler.id("effect_id").toString())) {
+            Identifier.CODEC.decode(NbtOps.INSTANCE, tag[Swindler.id("effect_id").toString()])
+                .resultOrPartial()
+                .ifPresent { pair: Pair<Identifier, NbtElement> ->
+                    effectId = pair.first
+                }
+        }
+        if (tag.contains(Swindler.id("data").toString())) {
+            effectData = tag.getCompound(Swindler.id("data").toString())
+        }
+    }
+
+    override fun writeToNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
+        Identifier.CODEC.encodeStart(NbtOps.INSTANCE, effectId)
+            .resultOrPartial()
+            .ifPresent { element: NbtElement? -> tag.put(Swindler.id("effect_id").toString(), element) }
+        tag.put(Swindler.id("data").toString(), effectData)
+    }
 }
--- src/main/kotlin/mod/master_bw3/swindler/entity/SpellEmitterEntity.kt
+++ src/main/kotlin/mod/master_bw3/swindler/entity/SpellEmitterEntity.kt
@@ -1,8 +1,6 @@
 package mod.master_bw3.swindler.entity
 
-import com.mojang.datafixers.util.Pair
 import dev.enjarai.trickster.spell.SpellPart
-import mod.master_bw3.swindler.Swindler
 import mod.master_bw3.swindler.SwindlerCardinalComponents.SPELL_EFFECT
 import mod.master_bw3.swindler.SwindlerCardinalComponents.SPELL
 import mod.master_bw3.swindler.registry.Entities
@@ -12,33 +10,24 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.data.DataTracker
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.nbt.NbtElement
-import net.minecraft.nbt.NbtOps
-import net.minecraft.registry.RegistryWrapper
 import net.minecraft.util.Identifier
 import net.minecraft.world.World
-import org.ladysnake.cca.api.v3.component.Component
-import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent
+
 
 class SpellEmitterEntity(type: EntityType<SpellEmitterEntity>, world: World) : Entity(type, world) {
 
     var spell: SpellPart
         get() = SPELL.get(this).spell
-        set(value) {
-            SPELL.get(this).spell = value; SPELL.sync(this)
-        }
+        set(value) { SPELL.get(this).spell = value }
 
     var effectId: Identifier
         get() = SPELL_EFFECT.get(this).effectId
-        set(value) {
-            SPELL_EFFECT.get(this).effectId = value; SPELL_EFFECT.sync(this)
-        }
+        set(value) { SPELL_EFFECT.get(this).effectId = value }
 
     var effectData: NbtCompound
         get() = SPELL_EFFECT.get(this).effectData
-        set(value) {
-            SPELL_EFFECT.get(this).effectData = value; SPELL_EFFECT.sync(this)
-        }
+        set(value) { SPELL_EFFECT.get(this).effectData = value }
+
 
     var lifetime: Int = 0
 
@@ -68,49 +57,4 @@ class SpellEmitterEntity(type: EntityType<SpellEmitterEntity>, world: World) : E
         effectData = getEffect()?.onTickEffect(this, effectData) ?: effectData
     }
 
-}
-
-class SpellComponent : Component, AutoSyncedComponent {
-    var spell: SpellPart = SpellPart()
-
-    override fun readFromNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
-        if (tag.contains(Swindler.id("spell").toString())) {
-            SpellPart.CODEC.decode(NbtOps.INSTANCE, tag[Swindler.id("spell").toString()])
-                .resultOrPartial()
-                .ifPresent { pair: Pair<SpellPart, NbtElement> ->
-                    spell = pair.first
-                }
-        }
-    }
-
-    override fun writeToNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
-        SpellPart.CODEC.encodeStart(NbtOps.INSTANCE, spell)
-            .resultOrPartial()
-            .ifPresent { element: NbtElement? -> tag.put(Swindler.id("spell").toString(), element) }
-    }
-}
-
-class SpellEffectComponent : Component, AutoSyncedComponent {
-    var effectId: Identifier = Swindler.id("none")
-    var effectData: NbtCompound = NbtCompound()
-
-    override fun readFromNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
-        if (tag.contains(Swindler.id("effect_id").toString())) {
-            Identifier.CODEC.decode(NbtOps.INSTANCE, tag[Swindler.id("effect_id").toString()])
-                .resultOrPartial()
-                .ifPresent { pair: Pair<Identifier, NbtElement> ->
-                    effectId = pair.first
-                }
-        }
-        if (tag.contains(Swindler.id("data").toString())) {
-            effectData = tag.getCompound(Swindler.id("data").toString())
-        }
-    }
-
-    override fun writeToNbt(tag: NbtCompound, registryLookup: RegistryWrapper.WrapperLookup) {
-        Identifier.CODEC.encodeStart(NbtOps.INSTANCE, effectId)
-            .resultOrPartial()
-            .ifPresent { element: NbtElement? -> tag.put(Swindler.id("effect_id").toString(), element) }
-        tag.put(Swindler.id("data").toString(), effectData)
-    }
 }
